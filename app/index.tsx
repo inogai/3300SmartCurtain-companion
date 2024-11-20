04cb11a834edb25fb4a308079b0ef41d7c3fb518
inogai <inogai.sign@gmail.com>
2024-11-20 22:18:06 +0800

    feat: bluetooth classic android config good

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
 import * as React from 'react'
 import { View } from 'react-native'
+import RNBluetoothClassic, { BluetoothEventType } from 'react-native-bluetooth-classic'
 import { Button } from '~/components/ui/button'
 import {
   Card,
   CardContent,
 } from '~/components/ui/card'
+
 import { Text } from '~/components/ui/text'
 
 export default function Screen() {
+  const [device, setDevice] = React.useState(null)
+
+  async function loadPairedDevices() {
+    try {
+      console.log('Loading paired devices')
+      console.log(RNBluetoothClassic)
+      const paired = await RNBluetoothClassic.getBondedDevices()
+      console.log('Paired devices:', paired)
+    }
+    catch (err) {
+    // Error if Bluetooth is not enabled
+    // Or there are any issues requesting paired devices
+      console.error(err)
+    }
+  }
+
   return (
     <View className="flex-1 justify-startitems-start gap-5 p-6 bg-secondary/30">
+      <Card className="w-full p-6 rounded-2xl">
+        <CardContent>
+          <Button onPress={loadPairedDevices}>
+            <Text>
+              Load Paired Devices
+            </Text>
+          </Button>
+        </CardContent>
+
+      </Card>
       <Card className="w-full p-6 rounded-2xl">
         <CardContent>
           <View className="flex-row justify-around gap-3">
@@ -20,7 +48,9 @@ export default function Screen() {
             </View>
             <View className="items-center">
               <Text className="text-sm text-muted-foreground">Currently</Text>
-              <Text className="text-xl font-semibold">70</Text>
+              <Text className="text-xl font-semibold">
+                {device || 'None'}
+              </Text>
             </View>
             <View className="items-center">
               <Button>
